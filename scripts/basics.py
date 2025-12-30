da20d891c9412e5c3a0b465ede2991bf5cc6826e
Victoria Butrym <victoriabutrym@MacBook-Pro-Victoria.local>
2025-12-30 14:41:27 +0300

    basics

---
+++ scripts/basics.py
@@ -0,0 +1,17 @@
+print('hello')
+
+num = 14
+name = 'Aramaki'
+my_bool = True
+
+position_x, position_y = 4, 1
+
+type_of_user_name = type(name)
+
+print(position_x)
+print(position_y)
+
+# types
+print(type(my_bool))
+print(type(num))
+print(type_of_user_name)
