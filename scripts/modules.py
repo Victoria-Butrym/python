b30f2a78d3d97b516a0bda6440c51185e43ee5a8
Victoria Butrym <victoriabutrym@MacBook-Pro-Victoria.local>
2026-01-09 15:33:04 +0300

    modules basics

---
+++ scripts/modules.py
@@ -0,0 +1,16 @@
+import sys, os, platform
+
+from math import sqrt as s, ceil
+
+from mymodule import name, hello, sum_of_three_numbers as sum
+
+print(name)
+hello()
+print(sum(2, 1, -5))
+
+# print(sys.path)
+# print(os.name)
+# print(platform.system())
+
+# print(s(25))
+# print(ceil(5.34))
---
+++ scripts/mymodule.py
@@ -0,0 +1,7 @@
+name = 'Max Verstappen'
+
+def hello():
+    print('hello, name in Module: ' + name)
+
+def sum_of_three_numbers(a, b, c):
+    return a + b + c if (a != 0 and b != 0  and c != 0) else 'zero numbers are not allowed'
